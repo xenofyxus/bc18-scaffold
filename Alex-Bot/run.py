22da8c6db03f1717e0195e8b5efac22ab61c5ee9
Uberpapi <alex.lindstrom92@hotmail.com>
2019-04-16 17:16:53 +0200

    Basic worker and factory behavioir for testing

--- Alex-Bot/run.py
+++ Alex-Bot/run.py
@@ -3,11 +3,9 @@ import random
 import sys
 import traceback
 import time
-
 import os
 print(os.getcwd())
 
-print("pystarting")
 
 # A GameController is the main type that you talk to the game with.
 # Its constructor will connect to a running game.
@@ -29,50 +27,18 @@ gc.queue_research(bc.UnitType.Knight)
 
 my_team = gc.team()
 
-while True:
-    # We only support Python 3, which means brackets around print()
-    print('pyround:', gc.round(), 'time left:', gc.get_time_left_ms(), 'ms')
-
-    # frequent try/catches are a good idea
-    try:
-        # walk through our units:
-        for unit in gc.my_units():
-
-            # first, factory logic
-            if unit.unit_type == bc.UnitType.Factory:
-                FactoryTree(unit);
-
-            elif unit.unit_type == bc.UnitType.Worker:
-                WorkerTree(unit);
-
-            elif unit.unit_type == bc.UnitType.Knight:
-                KnightTree(unit);
-
-            elif unit.unit_type == bc.UnitType.Mage:
-                MageTree(unit);
-
-            elif unit.unit_type == bc.UnitType.Ranger:
-                RangerTree(unit);    
-                
-
-    except Exception as e:
-        print('Error:', e)
-        # use this to show where the error was
-        traceback.print_exc()
-
-    # send the actions we've performed, and wait for our next turn.
-    gc.next_turn()
-
-    # these lines are not strictly necessary, but it helps make the logs make more sense.
-    # it forces everything we've written this turn to be written to the manager.
-    sys.stdout.flush()
-    sys.stderr.flush()
 
 def FactoryTree(unit):
 
     try:
 
-        print("I am a factory")
+        if len(unit.structure_garrison()) > 0:
+            dir = random.choice(directions)
+            if(gc.can_unload(unit.id, dir)):
+                gc.unload(unit.id, dir)
+
+        elif gc.can_produce_robot(unit.id, bc.UnitType.Mage):
+            gc.produce_robot(unit.id, bc.UnitType.Mage)
 
     except Exception as e:
         print('Factory Error:', e)
@@ -83,8 +49,19 @@ def WorkerTree(unit):
 
     try:
 
+        nearbyObjects = gc.sense_nearby_units(unit.location.map_location(), 2)
+        dir = random.choice(directions)
 
-        print("I am a worker")
+        for object in nearbyObjects:
+            if gc.can_build(unit.id, object.id):
+                gc.build(unit.id, object.id)
+                continue
+        
+        if gc.can_blueprint(unit.id, bc.UnitType.Factory, dir):
+            gc.blueprint(unit.id, bc.UnitType.Factory, dir)
+
+        elif gc.is_move_ready(unit.id) and gc.can_move(unit.id, dir):
+            gc.move_robot(unit.id, dir)
 
     except Exception as e:
         print('Worker Error:', e)
@@ -126,5 +103,51 @@ def RangerTree(unit):
         print('Ranger Error:', e)
         # use this to show where the error was
         traceback.print_exc()
+
+
+print("pystarting")
+
+
+
+while True:
+    # We only support Python 3, which means brackets around print()
+    print('pyround:', gc.round(), 'time left:', gc.get_time_left_ms(), 'ms')
+
+    # frequent try/catches are a good idea
+    try:
+        # walk through our units:
+        for unit in gc.my_units():
+
+            # first, factory logic
+            if unit.unit_type == bc.UnitType.Factory:
+                FactoryTree(unit)
+
+            elif unit.unit_type == bc.UnitType.Worker:
+                WorkerTree(unit)
+
+            elif unit.unit_type == bc.UnitType.Knight:
+                KnightTree(unit)
+
+            elif unit.unit_type == bc.UnitType.Mage:
+                MageTree(unit)
+
+            elif unit.unit_type == bc.UnitType.Ranger:
+                RangerTree(unit)    
+                
+
+    except Exception as e:
+        print('Error:', e)
+        # use this to show where the error was
+        traceback.print_exc()
+
+    # send the actions we've performed, and wait for our next turn.
+    gc.next_turn()
+
+    # these lines are not strictly necessary, but it helps make the logs make more sense.
+    # it forces everything we've written this turn to be written to the manager.
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+
 
 
